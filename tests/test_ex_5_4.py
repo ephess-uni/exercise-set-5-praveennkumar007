773a838363017caf337d4283616ca9da133c684c
Forrest Smith <fasmith0229@gmail.com>
2023-05-08 05:47:38 -0500

    FIX: Issue with case-sensitive dirname

--- tests/test_ex_5_4.py
+++ tests/test_ex_5_4.py
@@ -2,13 +2,17 @@
 Tests for ex_5_4
 """
 from os import path, system
+import os
 import numpy as np
 
 DIR = path.dirname(__file__)
 INFILE = path.join(DIR, "../data/ex_5_4-data.csv")
 OUTFILE = path.join(DIR, "../outputs/ex_5_4-processed.csv")
 MODULE_PATH = path.join(DIR, "../src/ex_5_4.py")
+OUTPUT_DIR = path.join(DIR, "../outputs")
 
+if not path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR, mode=660)
 
 def test_ex_5_4_creates_correct_output_file():
     system(f'python3 "{MODULE_PATH}"')
